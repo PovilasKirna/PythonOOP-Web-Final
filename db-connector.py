441a9073de2151dcfd240cf612aab57fc88acfb2
PovilasKirna <povilas.kirna@gmail.com>
2020-05-13 20:56:29 +0300

    removed secrets.json

--- db-connector.py
+++ db-connector.py
@@ -7,9 +7,9 @@ class DbConnector():
         self.connection = self.makeConnection()
         self.tableTitle = title
         self.QueryDictionary = {
-            "Insert" : "INSERT INTO UserLoginData (Username, Passwd) VALUES (%s, %s)",
-            "ReturnAll" : "SELECT * FROM UserLoginData",
-            "Delete" : "DELETE FROM UserLoginData WHERE UserID = %s"
+            "Insert" : "INSERT INTO {} (Username, Passwd) VALUES (%s, %s)".format(self.tableTitle),
+            "ReturnAll" : "SELECT * FROM {}".format(self.tableTitle),
+            "Delete" : "DELETE FROM {} WHERE UserID = %s".format(self.tableTitle),
         }
     
     def getCredentials(self):
@@ -17,7 +17,7 @@ class DbConnector():
         with open('secrets.json') as f:
             self.credentials = json.load(f)
         return self.credentials
-
+    
     def makeConnection(self): 
         # Ensure your credentials were setup
         if self.credentials:
@@ -30,9 +30,9 @@ class DbConnector():
             )
         return self.connection
 
-    def runQuery(self, query, *args, **kwargs):
+    def runQuery(self, query, args):
         cursor = self.connection.cursor()
-        cursor.execute(query, *args, **kwargs)
+        cursor.execute(query, args)
         self.connection.commit()
         
     def returnQueryList(self, query):
@@ -45,8 +45,13 @@ class DbConnector():
 #Create Login object
 Login = DbConnector("UserLoginData")
 
-#Admin = ("Player 1", "password")
-#Login.runQuery(Login.QueryDictionary["Insert"], Admin)
+# Inserts a  created user
+# User = ("Player 1", "password")
+# Login.runQuery(Login.QueryDictionary["Insert"], User)
+
+#Deletes a user by key
+UserID = (11,)
+Login.runQuery(Login.QueryDictionary["Delete"], UserID)
 
 #Print all rows in UserLoginData 
 result = Login.returnQueryList(Login.QueryDictionary["ReturnAll"])
